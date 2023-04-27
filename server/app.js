cc4f5cd616abd16148db35997b32a12a6fd76e3a
Marques Smalley <marquessmalley@gmail.com>
2023-04-27 15:02:01 -0400

    backend cleanup

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { globalErrorHandling } = require("./controllers/errorController");
-const { authenticateUser } = require("./controllers/authController");
+
 const AppError = require("./utills/appError");
 const app = express();
 
@@ -21,14 +21,12 @@ app.use(express.json());
 app.use(cors());
 
 const userRoutes = require("./routes/user");
-const productsRoutes = require("./routes/product.js");
 
 // app.get("/", authenticateUser);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 app.use("/account", userRoutes);
-app.use("/products", productsRoutes);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find the requested url: ${req.url}`, 400));
